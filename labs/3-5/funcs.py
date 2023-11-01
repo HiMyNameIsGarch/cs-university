28f521ffb16719d032a3accfce9424effd3456f0
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2023-11-02 00:55:25 +0200

    write documentation

--- labs/3-5/funcs.py
+++ labs/3-5/funcs.py
@@ -2,22 +2,47 @@ from copy import deepcopy
 
 vs_list = []
 
+# This function will append a new list to the versions list (usefull for the undo operation)
+# Params: a list to be added in the list
+# Return: None, as it just adds a new list to the list
+# Dependencies: deepcopy function from copy module
 def append_version(lst:list):
     vs_list.append(deepcopy(lst))
 
+# This function will verify if the given value is a valid number or not
+# Meaning that it has to be between 10 and 100
+# Params: val as an integer
+# Returns: a boolean
 def is_value_valid(val):
     return val <= 100 and val >= 10
 
+# Verify if the indexes are valid for the given list.
+# The first index should be greater than 0, and the final index should be within the list's length.
+# Params:
+#   - lst: the list to be checked
+#   - fidx: the first index
+#   - tidx: the final index
+# Returns: a boolean indicating if the indexes are valid
 def is_index_valid(lst, fidx, tidx):
     return len(lst) >= tidx + 1 and fidx > 0 and fidx <= tidx
 
-# 1. Add the result of a new participant to the array
+# Add the result of a new participant to the array.
+# Params:
+#   - my_list: the list of participants
+#   - value: the value to be added as a new participant
+# Returns: the modified list with the new participant added
 def add(my_list:list, value):
     if not is_value_valid(value): return my_list
     my_list.append(value)
     append_version(my_list)
     return my_list
 
+# Insert a new value at the specified index in the list.
+# Params:
+#   - lst: the list of participants
+#   - idx: the index where the value should be inserted
+#   - val: the value to be inserted
+# Returns: the modified list with the new value inserted
 def insert(lst:list, idx, val):
     if not is_value_valid(val) or idx < 1: return lst
     if idx + 1 > len(lst):
@@ -27,42 +52,77 @@ def insert(lst:list, idx, val):
     append_version(lst)
     return lst
 
-# 2. Modify the scores in the array (as a result of appeals)
+# Remove the element at the specified index from the list.
+# Params:
+#   - lst: the list of participants
+#   - idx: the index of the element to be removed
+# Returns: the modified list with the element removed
 def remove(lst:list, idx): # removes the element at idx
     if not is_index_valid(lst, idx, idx): return lst
     del lst[idx - 1]
     append_version(lst)
     return lst
 
+# Remove elements within the specified index range from the list.
+# Params:
+#   - lst: the list of participants
+#   - fidx: the starting index of removal
+#   - tidx: the ending index of removal
+# Returns: the modified list with elements removed in the specified range
 def remove_int(lst:list, fidx, tidx): # removes elements between the two given idx values
     if not is_index_valid(lst, fidx, tidx): return lst
     del lst[fidx - 1:tidx]
     append_version(lst)
     return lst
 
+# Replace the score at the specified index with a new value.
+# Params:
+#   - lst: the list of participants
+#   - idx: the index where the score should be replaced
+#   - nval: the new value to replace the score
+# Returns: the modified list with the score replaced
 def replace(lst:list, idx, nval): # replaces the score on idx with nval
     if not is_value_valid(nval) or not is_index_valid(lst, idx, idx): return lst
     lst[idx - 1] = nval
     append_version(lst)
     return lst
 
-# 3 Get the participants with scores having some properties
+# Get participants with scores less than or equal to a specified value.
+# Params:
+#   - lst: the list of participants
+#   - val: the value used for filtering
+# Returns: a new list of participants meeting the filter criteria
 def less(lst:list, val): # get participants with score less than val
     cp = deepcopy(lst)
     cp = sorted([i for i in cp if i <= val])
     return cp
 
+# Get a sorted list of all participants by their score.
+# Params:
+#   - lst: the list of participants
+# Returns: a new list of participants sorted by their scores in ascending order
 def sorted(lst:list): # get all participants sorted by their score
     cp = deepcopy(lst)
     cp.sort()
     return cp
 
+# Get the participants with scores higher than or equal to a specified value.
+# Params:
+#   - lst: the list of participants
+#   - val: the threshold value for filtering
+# Returns: a new list of participants meeting the filter criteria
 def sorted_by(lst:list, val): # get the participants with scores higher than val
     cp = deepcopy(lst)
     cp = [i for i in cp if i >= val]
     return cp
 
-# 4 Obtain different characteristics of participants
+# Calculate the average score of participants within a specified index range.
+# Params:
+#   - lst: the list of participants
+#   - fidx: the starting index for the average calculation
+#   - tidx: the ending index for the average calculation
+# Returns: the average score of participants within the specified range
+#          or 0 if the indices are not valid
 def avg(lst:list, fidx, tidx):
     if not is_index_valid(lst, fidx, tidx-1): return 0
     avg = 0
@@ -70,32 +130,58 @@ def avg(lst:list, fidx, tidx):
         avg += elem
     return avg // (tidx - fidx + 1)
 
-
+# Find the minimum score of participants within a specified index range.
+# Params:
+#   - lst: the list of participants
+#   - fidx: the starting index for finding the minimum score
+#   - tidx: the ending index for finding the minimum score
+# Returns: the minimum score of participants within the specified range
+#          or 0 if the indices are not valid
 def min(lst:list, fidx, tidx):
     if not is_index_valid(lst, fidx, tidx-1): return 0
     nlst = lst[fidx-1:tidx]
     nlst.sort()
     return nlst[0]
 
+# Filter and return a list of participants whose scores are divisible by a specified value
+# within a specified index range.
+# Params:
+#   - lst: the list of participants
+#   - mul: the divisor for filtering
+#   - fidx: the starting index for filtering
+#   - tidx: the ending index for filtering
+# Returns: a new list of participants meeting the filter criteria
+#          or an empty list if the indices are not valid
 def mul(lst:list, mul, fidx, tidx):
     if not is_index_valid(lst, fidx, tidx-1): return []
     nlst = lst[fidx-1:tidx]
     cp = [i for i in nlst if i % mul == 0]
     return cp
 
-# 5 Filter Values
+# Filter and return a list of participants whose scores are divisible by a specified value.
+# Params:
+#   - lst: the list of participants
+#   - mul: the divisor for filtering
+# Returns: a new list of participants meeting the filter criteria
+#          or an empty list if mul is less than 0
 def filter_mul(lst:list, mul):
     if mul < 0: return []
     nlst = [i for i in lst if i % mul == 0]
     append_version(nlst)
     return nlst
 
+# Filter and return a list of participants whose scores are greater than a specified value.
+# Params:
+#   - lst: the list of participants
+#   - greater: the threshold value for filtering
+# Returns: a new list of participants meeting the filter criteria
 def filter_greater(lst:list, greater):
     nlst = [i for i in lst if i > greater]
     append_version(nlst)
     return nlst
 
-# 6 Undo
+# Undo the most recent action by reverting to the previous version of the list.
+# Returns: the previous version of the list
 def undo():
     if len(vs_list) > 1: vs_list.pop()
     return vs_list[-1]
--- labs/3-5/tests.py
+++ labs/3-5/tests.py
@@ -1,147 +1,155 @@
 import funcs as f
+import inspect
 
-tlst = []
+def should_add():
+    tlst = []
+    tlst = f.add(tlst, 9)
+    assert len(tlst) == 0, "List should not be changed"
+    tlst = f.add(tlst, 101)
+    assert len(tlst) == 0, "List should not be changed"
+    tlst = f.add(tlst, 12)
+    assert len(tlst) > 0, "List should have been bigger"
 
-# Add
-tlst = f.add(tlst, 9)
-assert len(tlst) == 0, "List should not be changed"
-tlst = f.add(tlst, 101)
-assert len(tlst) == 0, "List should not be changed"
-tlst = f.add(tlst, 12)
-assert len(tlst) > 0, "List should have been bigger"
+def should_insert():
+    tlst = []
+    tlst = f.insert(tlst, 0, 99)
+    assert len(tlst) == 0, "List should not be changed by the invalid index"
+    idx = 2
+    tlst = f.insert(tlst, idx, 99)
+    assert len(tlst) == idx, "Length of the list should have been 2"
+    tlst = f.insert(tlst, 1, 101)
+    assert len(tlst) == idx, "List should not be changed by the invalid value"
 
-tlst = []
+def should_remove():
+    tlst = [23, 34] # Len = 2
+    tlst = f.remove(tlst, 0)
+    assert len(tlst) == 2, "List should not be changed by invalid index"
+    tlst = f.remove(tlst, 1)
+    assert len(tlst) == 1, "List should have been modified by the operation"
+    tlst = f.remove(tlst, 1000)
+    assert len(tlst) == 1, "List should not have been modified by the invalid index"
 
-# Insert
-tlst = f.insert(tlst, 0, 99)
-assert len(tlst) == 0, "List should not be changed by the invalid index"
-idx = 2
-tlst = f.insert(tlst, idx, 99)
-assert len(tlst) == idx, "Length of the list should have been 2"
-tlst = f.insert(tlst, 1, 101)
-assert len(tlst) == idx, "List should not be changed by the invalid value"
-
-tlst = [23, 34] # Len = 2
-
-# Remove
-tlst = f.remove(tlst, 0)
-assert len(tlst) == 2, "List should not be changed by invalid index"
-tlst = f.remove(tlst, 1)
-assert len(tlst) == 1, "List should have been modified by the operation"
-tlst = f.remove(tlst, 1000)
-assert len(tlst) == 1, "List should not have been modified by the invalid index"
-
-tlst = [23, 34, 56, 89] # Len = 2
 
 # Remove interval
-tlst = f.remove_int(tlst, -1, 2)
-assert len(tlst) == 4, "List should not have been modified by the invalid first index"
-tlst = f.remove_int(tlst, 1, 10)
-assert len(tlst) == 4, "List should not have been modified by the invalid final index"
-tlst = f.remove_int(tlst, 2, 3)
-assert len(tlst) == 2, "The operation should have removed just 2 elements"
+def should_remove_interval():
+    tlst = [23, 34, 56, 89] # Len = 2
+    tlst = f.remove_int(tlst, -1, 2)
+    assert len(tlst) == 4, "List should not have been modified by the invalid first index"
+    tlst = f.remove_int(tlst, 1, 10)
+    assert len(tlst) == 4, "List should not have been modified by the invalid final index"
+    tlst = f.remove_int(tlst, 2, 3)
+    assert len(tlst) == 2, "The operation should have removed just 2 elements"
 
-tlst = [23, 34, 56]
 
 # Replace
-tlst = f.replace(tlst, -1, 67)
-assert len(tlst) == 3, "List should not have been modified by the invalid index"
-nval = 34
-idx = 1
-tlst = f.replace(tlst, idx, nval)
-assert tlst[idx] == nval, "The index value should have been modified"
-tlst = f.replace(tlst, idx, 1000)
-assert tlst[idx] == nval, "The index value should not have been modified by the wrong value"
+def should_replace():
+    tlst = [23, 34, 56]
+    tlst = f.replace(tlst, -1, 67)
+    assert len(tlst) == 3, "List should not have been modified by the invalid index"
+    nval = 34
+    idx = 1
+    tlst = f.replace(tlst, idx, nval)
+    assert tlst[idx] == nval, "The index value should have been modified"
+    tlst = f.replace(tlst, idx, 1000)
+    assert tlst[idx] == nval, "The index value should not have been modified by the wrong value"
 
-tlst = [11, 45, 23]
 
-# Less
-nlst = f.less(tlst, 30)
-assert [i for i in nlst if i <= 30] == [11, 23], "Should return the only 2 elements less than 30"
-nlst = f.less(tlst, 9)
-assert [i for i in nlst if i <= 9] == [], "Should return an empty list"
-nlst = f.less(tlst, 100)
-assert [i for i in nlst if i <= 100] == [11, 23, 45], "Should return the entire list sorted"
+def should_less():
+    tlst = [11, 45, 23]
+    nlst = f.less(tlst, 30)
+    assert [i for i in nlst if i <= 30] == [11, 23], "Should return the only 2 elements less than 30"
+    nlst = f.less(tlst, 9)
+    assert [i for i in nlst if i <= 9] == [], "Should return an empty list"
+    nlst = f.less(tlst, 100)
+    assert [i for i in nlst if i <= 100] == [11, 23, 45], "Should return the entire list sorted"
 
-tlst = [11, 13, 12]
+def should_sort():
+    tlst = [11, 13, 12]
+    tlst = f.sorted(tlst)
+    assert tlst == [11,12,13], "Should return the sorted list"
+    tlst = [11,12]
+    tlst = f.sorted(tlst)
+    assert tlst == tlst, "Should return the list unmodified"
+    tlst = []
+    tlst = f.sorted(tlst)
+    assert tlst == [], "Should return the empty list"
 
-# Sorted
-tlst = f.sorted(tlst)
-assert tlst == [11,12,13], "Should return the sorted list"
-tlst = [11,12]
-tlst = f.sorted(tlst)
-assert tlst == tlst, "Should return the list unmodified"
-tlst = []
-tlst = f.sorted(tlst)
-assert tlst == [], "Should return the empty list"
+def should_sort_by():
+    tlst = [11, 56, 34]
+    nlst = f.sorted_by(tlst,101)
+    assert nlst == [], "Should return an empty list, value out of bounds"
+    nlst = f.sorted_by(tlst, 34)
+    assert nlst == [56,34], "Should return 2 elements that follows the rules"
+    nlst = f.sorted_by(tlst, 10)
+    assert nlst == tlst, "Should return the entire list"
 
-tlst = [11, 56, 34]
-
-# Sorted_by
-nlst = f.sorted_by(tlst,101)
-assert nlst == [], "Should return an empty list, value out of bounds"
-nlst = f.sorted_by(tlst, 34)
-assert nlst == [56,34], "Should return 2 elements that follows the rules"
-nlst = f.sorted_by(tlst, 10)
-assert nlst == tlst, "Should return the entire list"
-
-tlst = [12,13,14]
 
 # Avg
-avg = f.avg(tlst, 1,2)
-assert avg == 12, "Should return the average value of first 2 terms"
-avg = f.avg(tlst, 2,1)
-assert avg == 0, "Should not return the average of wrong indexes"
-avg = f.avg(tlst, -12, 45)
-assert avg == 0, "Should not return the average of indexes out of bounds"
+def should_avg():
+    tlst = [12,13,14]
+    avg = f.avg(tlst, 1,2)
+    assert avg == 12, "Should return the average value of first 2 terms"
+    avg = f.avg(tlst, 2,1)
+    assert avg == 0, "Should not return the average of wrong indexes"
+    avg = f.avg(tlst, -12, 45)
+    assert avg == 0, "Should not return the average of indexes out of bounds"
 
-tlst = [56, 12, 34]
+def should_min():
+    tlst = [56, 12, 34]
+    min = f.min(tlst, 1, 3)
+    assert min == 12, "Should return the minimum value of the list"
+    min = f.min(tlst, 3, 1)
+    assert min == 0, "Should return default value of 0 as of wrong indexes"
+    min = f.min(tlst, -1, 12)
+    assert min == 0, "Should return default value of 0 as of wrong indexes"
 
-# Min
-min = f.min(tlst, 1, 3)
-assert min == 12, "Should return the minimum value of the list"
-min = f.min(tlst, 3, 1)
-assert min == 0, "Should return default value of 0 as of wrong indexes"
-min = f.min(tlst, -1, 12)
-assert min == 0, "Should return default value of 0 as of wrong indexes"
-
-tlst = [12, 55, 34, 89]
 
 # Mul
-mul_lst = f.mul(tlst, 2, 1, 4)
-assert mul_lst == [12, 34], "Should return only the elements that are multiple of 2"
-mul_lst = f.mul(tlst, 2, -1, 10)
-assert mul_lst == [], "Should return empty list as of wrong indexes"
-mul_lst = f.mul(tlst, 13, 1,2)
-assert mul_lst == [], "Should return empty list as there are no multiples of 13 in the list"
+def should_mul():
+    tlst = [12, 55, 34, 89]
+    mul_lst = f.mul(tlst, 2, 1, 4)
+    assert mul_lst == [12, 34], "Should return only the elements that are multiple of 2"
+    mul_lst = f.mul(tlst, 2, -1, 10)
+    assert mul_lst == [], "Should return empty list as of wrong indexes"
+    mul_lst = f.mul(tlst, 13, 1,2)
+    assert mul_lst == [], "Should return empty list as there are no multiples of 13 in the list"
 
 # Filter_mul
-mul_lst = f.filter_mul(tlst, 10)
-assert mul_lst == [], "Should return the only elements mul of 10, which are none"
-mul_lst = f.filter_mul(tlst, 2)
-assert mul_lst == [12, 34], "Should return the only elements mul of 2"
-mul_lst = f.filter_mul(tlst, -1)
-assert mul_lst == [], "Should return empty list as of out of bound value"
+def should_filter_mul():
+    tlst = [12, 55, 34, 89]
+    mul_lst = f.filter_mul(tlst, 10)
+    assert mul_lst == [], "Should return the only elements mul of 10, which are none"
+    mul_lst = f.filter_mul(tlst, 2)
+    assert mul_lst == [12, 34], "Should return the only elements mul of 2"
+    mul_lst = f.filter_mul(tlst, -1)
+    assert mul_lst == [], "Should return empty list as of out of bound value"
 
 # Filter_greater
-grt_lst = f.filter_greater(tlst, 10)
-assert grt_lst == tlst, "Should return the entire list unmodified"
-grt_lst = f.filter_greater(tlst, 50)
-assert grt_lst == [55, 89], "Should return the only elements greater than 50"
-grt_lst = f.filter_greater(tlst, 101)
-assert grt_lst == [], "Should return empty list as of out of bound value"
+def should_filter_greater():
+    tlst = [12, 55, 34, 89]
+    grt_lst = f.filter_greater(tlst, 10)
+    assert grt_lst == tlst, "Should return the entire list unmodified"
+    grt_lst = f.filter_greater(tlst, 50)
+    assert grt_lst == [55, 89], "Should return the only elements greater than 50"
+    grt_lst = f.filter_greater(tlst, 101)
+    assert grt_lst == [], "Should return empty list as of out of bound value"
 
-f.vs_list = []
-tlst = [12,14]
-f.append_version(tlst)
 
 # Undo
-nlst = f.undo()
-assert nlst == tlst, "Should not do the undo operation as it is the only element in the version list"
-f.append_version(f.add(tlst, 12))
-nlst = f.undo()
-assert nlst == tlst, "Should be the first version of the list as it had been performed just one add operation"
-f.append_version(f.add(tlst, 34))
-f.append_version(f.add(tlst, 9))
-nlst = f.undo()
-assert nlst == [12,14,12,34], "Should mimic the last version of the list"
+def should_undo():
+    f.vs_list = []
+    tlst = [12,14]
+    f.append_version(tlst)
+    nlst = f.undo()
+    assert nlst == tlst, "Should not do the undo operation as it is the only element in the version list"
+    f.append_version(f.add(tlst, 12))
+    nlst = f.undo()
+    assert nlst == tlst, "Should be the first version of the list as it had been performed just one add operation"
+    f.append_version(f.add(tlst, 34))
+    f.append_version(f.add(tlst, 9))
+    nlst = f.undo()
+    assert nlst == [12,14,12,34], "Should mimic the last version of the list"
+
+
+all_functions = inspect.getmembers(__name__, inspect.isfunction)
+print(all_functions)
