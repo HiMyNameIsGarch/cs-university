842949bfc8cd36410c227542e46c18e011f2559a
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2023-11-14 03:08:47 +0200

    new lecture just dropped

---
+++ lectures/5/logic.py
@@ -0,0 +1,15 @@
+def increase_by(val):
+    return val
+
+def top_k():
+    return "asd"
+
+def highest():
+    return "asd"
+
+def sort_points():
+    return 1
+
+def test_distance():
+    point1 = [1,1]
+    point2 = [2,5]
---
+++ lectures/5/main.py
@@ -0,0 +1,8 @@
+pts = [[1,2], [5,4], [3,2]]
+
+def sort_points():
+    return sorted(pts, key=lambda pt: pt[0])
+
+
+print(sort_points())
+
