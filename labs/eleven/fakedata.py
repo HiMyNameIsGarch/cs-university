555967e862d91587599b4e8f0082614dea871256
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2023-12-19 18:09:31 +0200

    fake data improved

--- labs/eleven/fakedata.py
+++ labs/eleven/fakedata.py
@@ -3,6 +3,7 @@ from faker import Faker
 from domain.passenger import Passenger
 from domain.plane import Plane
 from faker_airtravel import AirTravelProvider
+from concurrent.futures import ProcessPoolExecutor
 
 fake = Faker()
 fake.add_provider(AirTravelProvider)
@@ -31,3 +32,10 @@ def generate_planes(num_planes, num_passengers_per_plane):
             ipassengers=generate_passenger(num_passengers_per_plane))
         planes.append(cplane)
     return planes
+
+
+# Experimental, don't touch
+def gen_threads():
+     with ProcessPoolExecutor(8) as exe:
+        results = exe.map(generate_planes, [10000000]*8, [100]*8)
+        return list(results)
