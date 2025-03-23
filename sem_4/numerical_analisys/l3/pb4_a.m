61fb9b35572eef1ddb290d6687b6a2706e8894df
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2025-03-23 18:13:42 +0200

    remove the plots ( or maybe not?)

--- sem_4/numerical_analisys/l3/pb4_a.m
+++ sem_4/numerical_analisys/l3/pb4_a.m
@@ -10,18 +10,7 @@ y_i = (0:0.01:100)/10 - 5;
 
 len = length(n_values);
 
-% Compute line and column from the n_values vector
-try
-    rows = ceil(sqrt(len));
-    cols = ceil(len / rows);
-catch
-    rows = 2;
-    cols = 2;
-end
-
-figure;
-
-for k = 1:length(n_values)
+for k = 1:len
     n = n_values(k);
 
     % x and y nodes
@@ -36,27 +25,7 @@ for k = 1:length(n_values)
         y_interp(m) = lagrange_interpolation(x_nodes, y_nodes, y_i(m));
     end
 
-    % Max interpolation error
+    % max interpolation error
     E_n = max(abs(f(y_i) - y_interp));
     fprintf('n = %d, max error: %.6f\n', n, E_n);
-
-    % plotting time
-    subplot(rows, cols, k);
-
-    % original function
-    plot(y_i, f(y_i), 'b-', 'LineWidth', 2);
-    hold on;
-
-    % lagrange ( red )
-    plot(y_i, y_interp, 'r--', 'LineWidth', 2);
-
-    % interpolation ( black )
-    plot(x_nodes, y_nodes, 'ko', 'MarkerFaceColor', 'k');
-
-    % usefull informations
-    title(sprintf('n = %d, E_n = %.6f', n, E_n));
-    legend('f(x)', 'P_n(x)', 'Nodes');
-
-    grid on;
-    hold off;
 end
--- sem_4/numerical_analisys/l3/pb4_b.m
+++ sem_4/numerical_analisys/l3/pb4_b.m
@@ -10,24 +10,19 @@ y_i = (0:0.01:100)/10 - 5;
 
 len = length(n_values);
 
-% Compute line and column from the n_values vector
-[rows, cols] = rowscols(len);
-
 i = 0:20;
-
-figure;
 
 for k = 1:len
     n = n_values(k);
 
     % x_i
-    % x_nodes_v1 = 5 * cos(i * pi / 20);
-    x_nodes_v1 = 5 * cos((0:n) * pi / n);
+    x_nodes_v1 = 5 * cos(i * pi / 20);
+    % x_nodes_v1 = 5 * cos((0:n) * pi / n);
     y_nodes_v1 = f(x_nodes_v1);
 
     % x_i2
-    % x_nodes_v2 = 5 * cos((2 * i + 1) * pi / 42);
-    x_nodes_v2 = 5 * cos((2*(0:n) + 1) * pi / (2*(n + 1)));
+    x_nodes_v2 = 5 * cos((2 * i + 1) * pi / 42);
+    % x_nodes_v2 = 5 * cos((2*(0:n) + 1) * pi / (2*(n + 1)));
     y_nodes_v2 = f(x_nodes_v2);
 
     % fill with zeros
@@ -42,30 +37,4 @@ for k = 1:len
     E_n_v1 = max(abs(f(y_i) - y_interp_v1));
     E_n_v2 = max(abs(f(y_i) - y_interp_v2));
     fprintf('n = %d, max err v1: %.6f, max err v2: %.6f\n', n, E_n_v1, E_n_v2);
-
-    % subplotting
-    subplot(rows, cols, k);
-
-    % original function
-    plot(y_i, f(y_i), 'b-', 'LineWidth', 2);
-    hold on;
-
-    % langrange with x_i ( red )
-    plot(y_i, y_interp_v1, 'r--', 'LineWidth', 2);
-
-    % lagrnage with x_i2 ( green )
-    plot(y_i, y_interp_v2, 'g--', 'LineWidth', 2);
-
-    % interpolation ( black )
-    plot(x_nodes_v1, y_nodes_v1, 'ko', 'MarkerFaceColor', 'k');
-
-    % interpolation ( magenta )
-    plot(x_nodes_v2, y_nodes_v2, 'mo', 'MarkerFaceColor', 'm');
-
-    % usefull informations
-    title(fprintf('n = %d, max err v1: %.6f, max err v2: %.6f\n', n, E_n_v1, E_n_v2));
-    legend('f(x)', 'P_n(x) v1', 'P_n(x) v2', 'Nodes v1', 'Nodes v2');
-
-    grid on;
-    hold off;
 end
