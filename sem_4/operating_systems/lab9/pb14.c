95793e233b0ade30e7d8f840339e0b4973e1e804
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2025-05-29 15:41:06 +0300

    add requirements for problem 14

---
+++ sem_4/operating_systems/lab9/pb14.c
@@ -0,0 +1,16 @@
+// Implement the following structure of processes,
+//     in which the vertices indicate the communication between
+//         processes using pipes:
+
+
+
+//   1
+//  / \
+// 2   3
+//  \
+//   4
+
+
+// Parent process sends to the child a filename,
+//     and the child sends back to the parent the content of the file or
+//         an error message if the file does not exist.
